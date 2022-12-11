22a92c8377ee9d9dfc2af6f16b36a04a1ab5dd2a
eart4 <metrika987@gmail.com>
2022-12-11 23:53:14 +0300

    added carousel

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-$(function(){
-
-});
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        center: true,
+        loop: true,
+        margin: 30,
+        startPosition: 1,
+        items: 3,
+    });
+  });

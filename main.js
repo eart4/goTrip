7955953a723cf9c9b8ed2c655d6d08073aee6376
eart4 <metrika987@gmail.com>
2022-12-12 19:50:38 +0300

    ended desktop version

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        center: true,
-        loop: true,
-        margin: 30,
-        startPosition: 1,
-        items: 3,
-    });
-  });
+
+
+
+
+
+
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    center: true,
+    loop: true,
+    margin: 30,
+    startPosition: 1,
+    items: 3,
+});
+
+$('.slider__btn--next').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+
+$('.slider__btn--prev').click(function() {
+
+    owl.trigger('prev.owl.carousel', [300]);
+})
